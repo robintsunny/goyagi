7e74aa65e6b00d61f35147b715cf1c0d5b2ef0b5
Robin Sunny <robinsunny@Robins-MacBook-Pro.local>
2019-02-08 15:14:08 -0800

    signals

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -1,11 +1,10 @@
 package main
 
-
 import (
 	"net/http"
 
-	"github.com/robintsunny/goyagi/pkg/server"
 	"github.com/lob/logger-go"
+	"github.com/robintsunny/goyagi/pkg/server"
 )
 
 func main() {
@@ -16,8 +15,7 @@ func main() {
 	log.Info("server started")
 
 	err := srv.ListenAndServe()
-
-	i err != nil && err != http.ErrServerClosed {
+	if err != nil && err != http.ErrServerClosed {
 		log.Err(err).Fatal("server stopped")
 	}
 
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,19 +1,34 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/lob/logger-go"
+	"github.com/robintsunny/goyagi/pkg/signals"
 )
 
 func New() *http.Server {
+	log := logger.New()
+
 	e := echo.New()
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", 3000),
 		Handler: e,
 	}
+
+	graceful := signals.Setup()
+
+	go func() {
+		<-graceful
+		err := srv.Shutdown(context.Background())
+		if err != nil {
+			log.Err(err).Error("server shutdown")
+		}
+	}()
 
 	return srv
 }
