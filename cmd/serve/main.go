51b0da81a0fec05d195cdad620dae731f51802f0
Robin Sunny <robinsunny@Robins-MacBook-Pro.local>
2019-02-13 16:33:38 -0800

    server

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -4,15 +4,18 @@ import (
 	"net/http"
 
 	"github.com/lob/logger-go"
+	"github.com/robintsunny/go/pkg/application"
 	"github.com/robintsunny/goyagi/pkg/server"
 )
 
 func main() {
 	log := logger.New()
 
+	app := application.New()
+
 	srv := server.New()
 
-	log.Info("server started")
+	log.Info("server started", logger.Data{"port": app.Config.Port})
 
 	err := srv.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
